2e49633501215533cfb35adf842aef368490831c
Frank Saar <frank.saar@gmail.com>
2017-12-21 21:16:40 +0400

    added test target

--- Package.swift
+++ Package.swift
@@ -6,10 +6,20 @@ import PackageDescription
 let package = Package(
     name: "filesize",
     dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", from:"1.2.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from:"7.0.3"),
     ],
     targets: [
+        
         .target(
             name: "filesize",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "filesizeTests",
+            dependencies: ["filesize"],
+            path: "Tests"
+        )
     ]
 )
---
+++ Tests/filesizeTests/filesizeTests.swift
@@ -0,0 +1,10 @@
+import Foundation
+import XCTest
+
+@testable import filesize
+
+class filesizeTests : XCTestCase {
+    func testCreatingFile() throws {
+        XCTAssertNotNil(nil)
+    }
+}
