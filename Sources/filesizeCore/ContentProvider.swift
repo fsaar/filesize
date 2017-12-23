324790a8074d6ab4466804b83b42160e3758753d
Frank Saar <frank.saar@gmail.com>
2017-12-23 20:40:53 +0400

    revised generator/sequence : moved to anygenerator for simplicity reasons

--- Sources/filesizeCore/ContentProvider.swift
+++ Sources/filesizeCore/ContentProvider.swift
@@ -7,47 +7,28 @@
 
 import Foundation
 
-public typealias ContentType = String
-public typealias Content = String
-public protocol ContentProvider {
-    var contentList : [(ContentType,Content)] { get }
-}
-
-
-public struct FileContentGenerator : IteratorProtocol {
+public class FileContentProvider{
     let urls : [URL]
     var currentIndex = 0
-    init?(with baseURL: URL) {
+    public init?(with baseURL: URL) {
         guard let enumerator = FileManager.default.enumerator(at: baseURL, includingPropertiesForKeys: [URLResourceKey.isDirectoryKey], options: [.skipsPackageDescendants,.skipsHiddenFiles],errorHandler: { _,_ in return true }) else {
             return nil
         }
         urls =  enumerator.flatMap({ $0 as? URL })
     }
-    
-    public mutating func next() -> (path:String,content:String)? {
-        guard currentIndex < urls.count else {
-            return nil
-        }
-        let url = urls[currentIndex]
-        let content = (try? String(contentsOf: url)) ?? ""
-        let value = (url.path, content)
-        currentIndex += 1
-        return value
-    }
-}
-
-public class FileContentProvider{
-    let fileContentGenerator : FileContentGenerator
-    public init?(with baseURL: URL) {
-        guard let generator = FileContentGenerator(with: baseURL) else {
-            return nil
-        }
-        fileContentGenerator = generator
-    }
 }
 
 extension FileContentProvider : Sequence {
-    public func makeIterator() -> FileContentGenerator {
-        return fileContentGenerator
+    public func makeIterator() -> AnyIterator<(path:String,content:String)> {
+        return AnyIterator<(path:String,content:String)> {
+            guard self.currentIndex < self.urls.count else {
+                return nil
+            }
+            let url = self.urls[self.currentIndex]
+            let content = (try? String(contentsOf: url)) ?? ""
+            let value = (url.path, content)
+            self.currentIndex += 1
+            return value
+        }
     }
 }
