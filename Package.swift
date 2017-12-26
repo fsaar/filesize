92234acbdc92935086d55f55f0e167d89d26f799
Frank Saar <frank.saar@gmail.com>
2017-12-26 18:41:54 +0400

    revised packages

--- Package.swift
+++ Package.swift
@@ -6,8 +6,6 @@ import PackageDescription
 let package = Package(
     name: "filesize",
     dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", from:"1.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from:"7.0.3"),
     ],
     targets: [
         
